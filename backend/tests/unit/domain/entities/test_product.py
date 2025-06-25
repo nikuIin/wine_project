ee0f61416e68c80fd8b0fbfd523fee759e848366
van <vexyzy@yandex.ru>
2025-06-25 18:17:39 +0400

    test: add test 'product with discount'

--- backend/tests/unit/domain/entities/test_product.py
+++ backend/tests/unit/domain/entities/test_product.py
@@ -12,15 +12,21 @@ from tests.unit.domain.entities.conftest import (
 
 class TestProduct:
     def test_product_field_types(self, product_base):
-        assert isinstance(product_base.uuid, UUID)
+        assert isinstance(product_base.product_uuid, UUID)
         assert isinstance(product_base.name, str)
         assert isinstance(product_base.price, float)
         assert isinstance(product_base.brand_id, int)
         assert isinstance(product_base.discount, float)
 
     def test_product_creation_data(self, product_base):
-        assert product_base.uuid == TEST_PRODUCT_UUID
+        assert product_base.product_uuid == TEST_PRODUCT_UUID
         assert product_base.name == TEST_PRODUCT_NAME
         assert product_base.price == TEST_PRODUCT_PRICE
         assert product_base.brand_id == TEST_RPODUCT_BRAND_ID
         assert product_base.discount == TEST_PRODUCT_DISCOUNT
+
+    def test_product_price_with_discount(self, product_base):
+        assert (
+            product_base.price_with_discount
+            == TEST_PRODUCT_PRICE - TEST_PRODUCT_PRICE * TEST_PRODUCT_DISCOUNT
+        )
