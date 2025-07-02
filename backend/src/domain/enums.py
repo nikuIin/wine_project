0978e163722616d689b6598f75bde0e2da0914a9
van <vexyzy@yandex.ru>
2025-07-03 01:46:09 +0400

    feat: initialize language enums

---
+++ backend/src/domain/enums.py
@@ -0,0 +1,10 @@
+from enum import IntEnum, StrEnum
+
+
+class LanguageEnum(StrEnum):
+    # TODO: configure enum for really supported languages
+    DEFAULT_LANGUAGE = "ru"
+    ENGLISH = "en-US"
+    RUSSIAN = "ru"
+    RUSSIAN_MAT = "ru-mt"
+    GERMAN = "de"
