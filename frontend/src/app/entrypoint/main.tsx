a5c5cfc51ee22df6b14836180d114b84d3995e83
van <vexyzy@yandex.ru>
2025-08-22 14:53:24 +0400

    fix: move i8n configuration to entrypoint

--- frontend/src/app/entrypoint/main.tsx
+++ frontend/src/app/entrypoint/main.tsx
@@ -1,12 +1,38 @@
 import { App } from "@app/routes";
+import { ThemeProvider } from "@shared/index";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+import { ruBaseDictionary, enBaseDictionary, kzBaseDictionary, Languages } from "@shared/index";
+
+// Multingualism configuration
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    resources: {
+      [Languages.RUSSIAN]: { translation: { ...ruBaseDictionary } },
+      [Languages.ENGLISH]: { translation: { ...enBaseDictionary } },
+      [Languages.KAZAKHSTAN]: { translation: { ...kzBaseDictionary } },
+    },
+    fallbackLng: Languages.RUSSIAN,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    {/*Theme configuration*/}
+    <ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 );
--- frontend/src/app/routes/App.tsx
+++ frontend/src/app/routes/App.tsx
@@ -1,29 +1,50 @@
 import { Routes, Route } from "react-router-dom";
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
+import { useTranslation } from "react-i18next";
 
-import { ruBaseDictionary, enBaseDictionary, kzBaseDictionary, Languages } from "@shared/index";
+import { Languages } from "@shared/index";
+import { useEffect, useState } from "react";
+import i18next from "i18next";
 
-i18n
-  .use(initReactI18next)
-  .use(LanguageDetector)
-  .init({
-    resources: {
-      [Languages.RUSSIAN]: { translation: { ...ruBaseDictionary } },
-      [Languages.ENGLISH]: { translation: { ...enBaseDictionary } },
-      [Languages.KAZAKHSTAN]: { translation: { ...kzBaseDictionary } },
-    },
-    fallbackLng: Languages.RUSSIAN,
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 export function App() {
+  const { t } = useTranslation();
+
+  const [language, setLanguage] = useState<string>("ru");
+
+  useEffect(() => {
+    console.log(i18next.language);
+  }, [language]);
+
+  const changeLanguage = () => {
+    const random = getRandomInt(0, 2);
+    if (random == 0) {
+      i18next.changeLanguage(Languages.RUSSIAN);
+      setLanguage(Languages.RUSSIAN);
+    } else if (random == 1) {
+      i18next.changeLanguage(Languages.ENGLISH);
+      setLanguage(Languages.ENGLISH);
+    } else {
+      i18next.changeLanguage(Languages.KAZAKHSTAN);
+      setLanguage(Languages.KAZAKHSTAN);
+    }
+  };
+
   return (
     <Routes>
-      <Route path="/"></Route>
+      <Route
+        path="/"
+        element={
+          <div>
+            TEXT: {t("test")}
+            <button onClick={changeLanguage}></button>
+          </div>
+        }
+      ></Route>
     </Routes>
   );
 }
