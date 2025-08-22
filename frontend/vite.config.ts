051fd193d06844903c505fee3226988649524a06
van <vexyzy@yandex.ru>
2025-08-22 18:21:51 +0400

    chore: configure frontend configuration files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@shared": path.resolve(__dirname, "src/shared"),
       "@app": path.resolve(__dirname, "src/app"),
       "@entities": path.resolve(__dirname, "src/entities"),
+      "@widgets": path.resolve(__dirname, "src/widgets"),
+      "@pages": path.resolve(__dirname, "src/pages"),
     },
   },
 });
